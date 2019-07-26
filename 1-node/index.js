4f88db9025de26618a939f4f3eedf2472cfa88f7
Josephine Yao <jyao@SF-US17232-MBP13.local>
2019-07-26 15:44:06 -0500

    Done

--- 1-node/index.js
+++ 1-node/index.js
@@ -4,10 +4,14 @@ const mySecret = "afj239823u9843w189sj"
 
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJtZXNzYWdlIjoiWW91IEdvdCBUaGlzISA8MyJ9.ffRDp9RX5pLycpcQbIrg2l9BGM0xQC8Z2Pd90QeO40E"
 
-const message = null
+// const message = null
 
-if(message.constructor == String){
-    console.log( "Great Work")
-} else {
-    console.log( "Not finished yet...")
-}
+const result = jwt.decode(token, mySecret)
+
+console.log(result)
+
+// if(message.constructor == String){
+//     console.log( "Great Work")
+// } else {
+//     console.log( "Not finished yet...")
+// }
--- 1-ruby/index.rb
+++ 1-ruby/index.rb
@@ -4,10 +4,19 @@ my_secret = "afj239823u9843w189sj"
 
 token = "eyJhbGciOiJIUzI1NiJ9.eyJtZXNzYWdlIjoiWW91IEdvdCBUaGlzISA8MyJ9.ffRDp9RX5pLycpcQbIrg2l9BGM0xQC8Z2Pd90QeO40E"
 
-message = nil
+# message = nil
+
+# encoding = JWT.encode(payload, my_secret)
+# message = JWT.encode(payload, my_secret)
+
+result = JWT.decode(token, my_secret)
+
+puts result[0]
 
 if message.class == String
     puts "Great Work"
 else
     puts "Not finished yet..."
 end
+
+
